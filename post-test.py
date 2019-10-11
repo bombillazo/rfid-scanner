d6490e2c323efc2ea571b8f59f477fc7040cf2a7
Hector Ayala <44002321+bombillazo@users.noreply.github.com>
2019-10-11 13:56:50 -0400

    Update post-test.py

--- post-test.py
+++ post-test.py
@@ -1,6 +1,6 @@
 import requests
 url = 'http://172.31.10.24:3000/api/login'
 h = {'Content-Type': 'application/json',}
-d = '{"cardId": ""}'
+d = '{"cardId": "1234567"}'
 r = requests.post(url, headers=h, data=d)
 print(r.status_code)
